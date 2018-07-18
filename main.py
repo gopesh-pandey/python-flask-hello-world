789bbcd577cd060c9e9deffc3d4e3b5c1bdfbd3a
gopesh-pandey <30283512+gopesh-pandey@users.noreply.github.com>
2018-07-18 14:39:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 
 @app.route('/departments')
 def departments():
-  db = MySQLdb.connect(host="letstestdb.mysql.database.azure.com",user="myadmin@letstestdb",passwd="Welcome12@admin",db="mysampledb")
-  conn = db.cursor()
-  query = conn.execute("select distinct DEPARTMENT from salaries")
-	return {'departments': [i[0] for i in query.cursor.fetchall()]}
-  #return 'Hello, World!'
+  #db = MySQLdb.connect(host="letstestdb.mysql.database.azure.com",user="myadmin@letstestdb",passwd="Welcome12@admin",db="mysampledb")
+  #conn = db.cursor()
+  #query = conn.execute("select distinct DEPARTMENT from salaries")
+	#return {'departments': [i[0] for i in query.cursor.fetchall()]}
+  return 'Hello, World!'
 
 if __name__ == '__main__':
   app.run()
