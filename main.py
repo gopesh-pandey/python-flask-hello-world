e5a8f1f4991886aad474ee724890ed91f310b60c
gopesh-pandey <30283512+gopesh-pandey@users.noreply.github.com>
2018-07-18 15:41:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def listdept():
  db = MySQLdb.connect("letstestdb.mysql.database.azure.com","myadmin@letstestdb","Welcome12@admin","mysampledb" )
  # prepare a cursor object using cursor() method
  cursor = db.cursor()
- cursor.execute( "select * from salaries where Department='%s'"%department_name.upper())
- result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
- return result
- #return 'Hello, World...Will list db soon!'
+# cursor.execute( "select * from salaries where Department='%s'"%department_name.upper())
+# result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+# return result
+ return 'Hello, World...Will list db soon!'
 
 if __name__ == '__main__':
   app.run()
