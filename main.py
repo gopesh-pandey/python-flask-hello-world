8e265379d86dcc9c56de26f2db2eb0bb526b7aa0
gopesh-pandey <30283512+gopesh-pandey@users.noreply.github.com>
2018-07-18 16:30:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import MySQLdb
 
 app = Flask(__name__)
 
+
 @app.route('/departments')
 def listdept():
  # Open database connection
- #db = MySQLdb.connect("letstestdb.mysql.database.azure.com","myadmin@letstestdb","Welcome12@admin","mysampledb" )
- # prepare a cursor object using cursor() method
- #cursor = db.cursor()
-# cursor.execute( "select * from salaries where Department='%s'"%department_name.upper())
-# result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-# return result
- return 'Hello, World...Will list db soon!'
-
+ db = MySQLdb.connect("letstestdb.mysql.database.azure.com","myadmin@letstestdb","Welcome12@admin","mysampledb" )
+# prepare a cursor object using cursor() method
+ cursor = db.cursor()
+ cursor.execute( "select * from salaries where Department='%s'"%department_name.upper())
+ result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+ return result
+ #return 'Hello, World...Will list db soon!'
+ db.close()
 if __name__ == '__main__':
   app.run()
