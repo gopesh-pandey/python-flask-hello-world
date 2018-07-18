6c293b138f736d7d9ff72500a2ad8a175c130373
gopesh-pandey <30283512+gopesh-pandey@users.noreply.github.com>
2018-07-18 16:48:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from flask import Flask
-import MySQLdb
+from flask import Flask, jsonify
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+mysql = MySQL()
 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'myadmin@letstestdb'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Welcome12@admin'
+app.config['MYSQL_DATABASE_DB'] = 'mysampledb'
+app.config['MYSQL_DATABASE_HOST'] = 'letstestdb.mysql.database.azure.com'
 
-@app.route('/departments')
-def listdept():
- # Open database connection
- db = MySQLdb.connect("letstestdb.mysql.database.azure.com","myadmin@letstestdb","Welcome12@admin","mysampledb" )
-# prepare a cursor object using cursor() method
- cursor = db.cursor()
- cursor.execute( "select * from salaries where Department='%s'"%department_name.upper())
- result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
- return result
- #return 'Hello, World...Will list db soon!'
- db.close()
+mysql.init_app(app)
+
+@app.route('/v')
+def get():
+    cur = mysql.connect().cursor()
+    cur.execute('''select * from mysampledb.salaries''')
+    r = [dict((cur.description[i][0], value)
+              for i, value in enumerate(row)) for row in cur.fetchall()]
+    return jsonify({'myCollection' : r})
+
 if __name__ == '__main__':
-  app.run()
+    app.run()
