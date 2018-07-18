f11b1113e83fb7301d107dc99f9761a1daea94d0
gopesh-pandey <30283512+gopesh-pandey@users.noreply.github.com>
2018-07-18 15:42:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 @app.route('/departments')
 def listdept():
  # Open database connection
- db = MySQLdb.connect("letstestdb.mysql.database.azure.com","myadmin@letstestdb","Welcome12@admin","mysampledb" )
+ #db = MySQLdb.connect("letstestdb.mysql.database.azure.com","myadmin@letstestdb","Welcome12@admin","mysampledb" )
  # prepare a cursor object using cursor() method
- cursor = db.cursor()
+ #cursor = db.cursor()
 # cursor.execute( "select * from salaries where Department='%s'"%department_name.upper())
 # result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
 # return result
