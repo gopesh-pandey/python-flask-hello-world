e64a2a352eb5f8c397c2266f2e32fea8e5482952
gopesh-pandey <30283512+gopesh-pandey@users.noreply.github.com>
2018-07-19 14:06:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from flask import Flask, jsonify
-from flaskext.mysql import MySQL
+import MySQLdb
 
 app = Flask(__name__)
-mysql = MySQL()
 
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'myadmin@letstestdb'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Welcome12@admin'
-app.config['MYSQL_DATABASE_DB'] = 'mysampledb'
-app.config['MYSQL_DATABASE_HOST'] = 'letstestdb.mysql.database.azure.com'
+db = MySQLdb.connect(host="letstestdb.mysql.database.azure.com",user="myadmin@letstestdb",passwd="Welcome12@admin",db="mysampledb")
 
-mysql.init_app(app)
+cursor = db.cursor()
+
+# Execute SQL select statement
+
 
 @app.route('/v')
 def get():
-    cur = mysql.connect().cursor()
-    cur.execute('''select * from mysampledb.salaries''')
-    r = [dict((cur.description[i][0], value)
-              for i, value in enumerate(row)) for row in cur.fetchall()]
-    return jsonify({'myCollection' : r})
 
+    cursor.execute("SELECT distinct department FROM salaries")
+    print 'after query'
+    r = [dict((cursor.description[i][0], value)
+              for i, value in enumerate(row)) for row in cursor.fetchall()]
+    return jsonify({'myCollection' : r})
+    db.close()
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
