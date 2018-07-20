ecf747c53436fdaf7099669a8de48e81fcccf9a4
gopesh-pandey <30283512+gopesh-pandey@users.noreply.github.com>
2018-07-20 11:38:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-from flask import Flask, jsonify
-from flask import Flask, request
-import MySQLdb
-
+from flask import Flask
 app = Flask(__name__)
-
-db = MySQLdb.connect(host="letstestdb.mysql.database.azure.com",user="myadmin@letstestdb",passwd="Welcome12@admin",db="mysampledb")
-
-cursor = db.cursor()
-
  
-@app.route("/dept")
+@app.route("/")
 def hello():
-    #return "Hello World!"
-    cursor.execute("SELECT distinct department FROM salaries")
-    print 'after query'
-    r = [dict((cursor.description[i][0], value)
-              for i, value in enumerate(row)) for row in cursor.fetchall()]
-    return jsonify({'myCollection' : r})
-    db.close()
-
+    return "Hello World!"
+ 
 if __name__ == "__main__":
     app.run()
 	
